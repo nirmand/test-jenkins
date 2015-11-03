229da76cc2ed924428af06327cc0e153bbfc3c44
nirman.doshi <nirman123.doshi@gmail.com>
2015-11-03 14:07:46 +0530

    fail

--- CD_Jenkins/CD_Jenkins.Web.Tests/Controllers/HomeControllerTest.cs
+++ CD_Jenkins/CD_Jenkins.Web.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace CD_Jenkins.Web.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application. - I want this unit test to be failed", result.ViewBag.Message);
         }
 
         [TestMethod]
