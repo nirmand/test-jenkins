606ad08566ddf71f8f5aa144e65f17f54f8ed2d9
nirman.doshi <nirman123.doshi@gmail.com>
2015-11-05 12:33:21 +0530

    test

--- CD_Jenkins/CD_Jenkins.Web.Tests/Controllers/HomeControllerTest.cs
+++ CD_Jenkins/CD_Jenkins.Web.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace CD_Jenkins.Web.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application. - failed", result.ViewBag.Message);
         }
 
         [TestMethod]
